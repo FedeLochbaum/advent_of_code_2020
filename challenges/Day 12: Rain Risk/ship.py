63c96400715ee192dbcb85821d0212f76b674672
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-12-07 19:21:30 +0100

    day 12

--- challenges/Day 12: Rain Risk/ship.py
+++ challenges/Day 12: Rain Risk/ship.py
@@ -1,27 +1,22 @@
-from copy import deepcopy
 input_path = 'advent_of_code_2020/challenges/Day 12: Rain Risk/input'
 
 L = 'left'; R = 'right'
 N = 'north'; S = 'south'; E = 'east'; W = 'west'
-facing = 'facing_to'; pos = 'pos'
+waypoint = 'waypoint'; pos = 'pos'
 
 # I only keep the position of North and East, South is interpreted as - North and West as - East (as cartisians axis)
-state = { facing: E, pos: { N: 0, E: 0 } }
+state = { waypoint: { N: 1, E: 10 }, pos: { N: 0, E: 0 } }
 
-def incrementing_to_north(state, count): state[pos][N] += count
-def decrementing_to_north(state, count): state[pos][N] -= count
-def incrementing_to_east(state, count): state[pos][E] += count
-def decrementing_to_east(state, count): state[pos][E] -= count
+def incrementing_to_north(state, count): state[waypoint][N] += count
+def decrementing_to_north(state, count): state[waypoint][N] -= count
+def incrementing_to_east(state, count): state[waypoint][E] += count
+def decrementing_to_east(state, count): state[waypoint][E] -= count
 
-def follow_straight(state, count):
-  if state[facing] == N: return incrementing_to_north(state, count)
-  if state[facing] == S: return decrementing_to_north(state, count)
-  if state[facing] == E: return incrementing_to_east(state, count)
-  
-  return decrementing_to_east(state, count)
-
-def apply_rotation_map(_map, state, param): state[facing] = _map[param][state[facing]]
-def rotate_to(_map): return lambda state, param: apply_rotation_map(_map, state, param)
+def follow_straight(state, count): state[pos][N] += (count * state[waypoint][N]); state[pos][E] += (count * state[waypoint][E])
+def apply_rotation_map(_map, state, param):
+  north = state[waypoint][N]; east = state[waypoint][E]
+  state[waypoint][N] = _map[param][N](north, east); state[waypoint][E] = _map[param][E](north, east)
+def rotate_waypoint(_map): return lambda state, param: apply_rotation_map(_map, state, param)
 
 command_function = {
   'N': incrementing_to_north,
@@ -29,8 +24,16 @@ command_function = {
   'E': incrementing_to_east,
   'W': decrementing_to_east,
 
-  'L': rotate_to({ 90: { S: E, N: W, E: N, W: S }, 180: { S: N, N: S, E: W, W: E }, 270: { S: W, N: E, E: S, W: N } }),
-  'R': rotate_to({ 90: { S: W, N: E, E: S, W: N }, 180: { S: N, N: S, E: W, W: E }, 270: { S: E, N: W, E: N, W: S } }),
+  'R': rotate_waypoint({
+    90:  { N: lambda _, e: e * -1, E: lambda n, _: n },
+    180: { N: lambda n, _: n * -1, E: lambda _, e: e * -1 },
+    270: { N: lambda _, e: e, E: lambda n, _: n * -1 }
+  }),
+  'L': rotate_waypoint({
+    90: { N: lambda _, e: e, E: lambda n, _: n * -1 },
+    180: { N: lambda n, _: n * -1, E: lambda _, e: e * -1 },
+    270: { N: lambda _, e: e * -1, E: lambda n, _: n }
+  }),
 
   'F': follow_straight,
 }
